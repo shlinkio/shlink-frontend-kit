c9130938818fc36b551205efffd0289359bb2a6f
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-08-11 16:53:54 +0200

    Add skeleton of a dev page

--- dev/block/MessagePage.tsx
+++ dev/block/MessagePage.tsx
@@ -5,6 +5,8 @@ export const MessagePage: FC = () => (
   <>
     <Message>This is a message</Message>
     <Message type="error">Oops! This is an error</Message>
+    <Message fullWidth>Full width</Message>
     <Message loading />
+    <Message loading>Loading alternative content</Message>
   </>
 );
