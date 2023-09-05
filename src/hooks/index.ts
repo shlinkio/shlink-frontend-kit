0eaa22ce856d5f5079c6b76eb86fe02bc6dcb9bc
Alejandro Celaya <alejandrocelaya@gmail.com>
2023-09-05 17:46:09 +0200

    Add useTimeoutToggle hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -12,6 +12,32 @@ export const useToggle = (initialValue = false): ToggleResult => {
   return [flag, toggleFlag, setToTrue, setToFalse];
 };
 
+export type TimeoutToggle = typeof useTimeoutToggle;
+
+export const useTimeoutToggle = (
+  initialValue = false,
+  delay = 2000,
+
+  // Test seams
+  setTimeout = window.setTimeout,
+  clearTimeout = window.clearTimeout,
+): [boolean, () => void] => {
+  const [flag, setFlag] = useState<boolean>(initialValue);
+  const initialValueRef = useRef(initialValue);
+  const timeout = useRef<number>();
+  const callback = useCallback(() => {
+    setFlag(!initialValueRef.current);
+
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    timeout.current = setTimeout(() => setFlag(initialValueRef.current), delay);
+  }, [clearTimeout, delay, setTimeout]);
+
+  return [flag, callback];
+};
+
 export const useDomId = (): string => {
   const { current: id } = useRef(`dom-${uuid()}`);
   return id;
