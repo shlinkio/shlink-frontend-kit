14ec9041eed0a83d52deaee6fea0d013b0dffb0e
Alejandro Celaya <alejandrocelaya@gmail.com>
2024-07-22 18:32:17 +0200

    Remove dependency on QS package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,12 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org).
 
-## [Unreleased]
+## [0.5.2] - 2024-07-22
 ### Added
 * *Nothing*
 
 ### Changed
 * Update to `@shlinkio/eslint-config-js-coding-standard` 3.0, and migrate to ESLint flat config.
+* Drop dependency on `qs` and provide own query handling implementations.
 
 ### Deprecated
 * *Nothing*
--- test/utils/query.test.ts
+++ test/utils/query.test.ts
@@ -1,25 +1,27 @@
-import { parseQuery, stringifyQuery } from '../../src';
+import { parseQueryString, stringifyQueryParams } from '../../src';
 
 describe('query', () => {
-  describe('parseQuery', () => {
+  describe('parseQueryString', () => {
     it.each([
       ['', {}],
       ['foo=bar', { foo: 'bar' }],
       ['?foo=bar', { foo: 'bar' }],
       ['?foo=bar&baz=123', { foo: 'bar', baz: '123' }],
+      ['foo=bar&baz[]=123&baz[]=456', { foo: 'bar', baz: ['123', '456'] }],
     ])('parses query string as expected', (queryString, expectedResult) => {
-      expect(parseQuery(queryString)).toEqual(expectedResult);
+      expect(parseQueryString(queryString)).toEqual(expectedResult);
     });
   });
 
-  describe('stringifyQuery', () => {
+  describe('stringifyQueryParams', () => {
     it.each([
       [{}, ''],
       [{ foo: 'bar' }, 'foo=bar'],
       [{ foo: 'bar', baz: '123' }, 'foo=bar&baz=123'],
+      [{ foo: 'bar', ignored: undefined }, 'foo=bar'],
       [{ bar: 'foo', list: ['one', 'two'] }, encodeURI('bar=foo&list[]=one&list[]=two')],
     ])('stringifies query as expected', (queryObj, expectedResult) => {
-      expect(stringifyQuery(queryObj)).toEqual(expectedResult);
+      expect(stringifyQueryParams(queryObj)).toEqual(expectedResult);
     });
   });
 });
