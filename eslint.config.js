627ec8bddf1dc57183fec157e6b86c8eb7872f72
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-03-29 10:10:45 +0100

    Update coding standard

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,4 @@
 import shlink from '@shlinkio/eslint-config-js-coding-standard';
-import importPlugin from 'eslint-plugin-import';
-import reactCompiler from 'eslint-plugin-react-compiler';
 
 /* eslint-disable-next-line no-restricted-exports */
-export default [
-  ...shlink,
-  reactCompiler.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  {
-    rules: {
-      'import/no-duplicates': 'error',
-      'import/no-unresolved': 'off',
-    },
-  },
-];
+export default shlink;
