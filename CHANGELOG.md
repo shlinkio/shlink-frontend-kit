89fedf2e3e3756a6a0c2617feb147e4122ea1dd0
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-05-29 23:16:15 +0200

    Add sticky-cell tailwind utility

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,23 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org).
 
+## [Unreleased]
+### Added
+* Add `sticky-cell` tailwind utility.
+
+### Changed
+* *Nothing*
+
+### Deprecated
+* *Nothing*
+
+### Removed
+* *Nothing*
+
+### Fixed
+* *Nothing*
+
+
 ## [0.9.3] - 2025-05-09
 ### Added
 * *Nothing*
--- src/tailwind/form/CloseButton.tsx
+++ src/tailwind/form/CloseButton.tsx
@@ -6,15 +6,17 @@ import type { FC, HTMLProps } from 'react';
 export type CloseButtonProps = {
   label?: string;
   onClick?: HTMLProps<HTMLButtonElement>['onClick'];
+  className?: string;
 };
 
-export const CloseButton: FC<CloseButtonProps> = ({ onClick, label = 'Close' }) => (
+export const CloseButton: FC<CloseButtonProps> = ({ onClick, className, label = 'Close' }) => (
   <button
     type="button"
     onClick={onClick}
     className={clsx(
       'tw:opacity-50 tw:highlight:opacity-80 tw:transition-opacity',
       'tw:rounded-md tw:focus-ring',
+      className,
     )}
     aria-label={label}
   >
