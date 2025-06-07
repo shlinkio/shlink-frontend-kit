41c2e7c2b92605148cf8a9cf87abeb960830d628
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-06-07 09:53:34 +0200

    Support disabled menu items

--- src/tailwind/navigation/Menu.tsx
+++ src/tailwind/navigation/Menu.tsx
@@ -6,9 +6,10 @@ import { Card } from '../surfaces';
 
 export type MenuItemProps = {
   selected?: boolean;
+  disabled?: boolean;
 } & Omit<HTMLProps<HTMLButtonElement> | LinkProps, 'role'>;
 
-const Item: FC<MenuItemProps> = ({ className, selected, ...rest }) => {
+const Item: FC<MenuItemProps> = ({ className, selected, disabled, ...rest }) => {
   const Tag = 'to' in rest ? Link : 'button';
 
   return (
@@ -18,28 +19,33 @@ const Item: FC<MenuItemProps> = ({ className, selected, ...rest }) => {
       className={clsx(
         'tw:flex tw:items-center tw:w-full tw:px-3 tw:py-1.5 tw:focus-ring',
         {
-          'tw:bg-lm-secondary tw:dark:bg-dm-secondary': selected,
-          'tw:highlight:bg-lm-secondary tw:dark:highlight:bg-dm-secondary': !selected,
+          'tw:pointer-events-none tw:opacity-50': disabled,
+          'tw:bg-lm-secondary tw:dark:bg-dm-secondary': selected && !disabled,
+          'tw:highlight:bg-lm-secondary tw:dark:highlight:bg-dm-secondary': !selected && !disabled,
         },
         className,
       )}
       tabIndex={-1}
+      disabled={disabled}
+      aria-disabled={disabled}
       {...rest}
     />
   );
 };
 
 const Separator: FC = () => (
+  // TODO Use an <hr /> tag once tailwind styles are not set with !important
   <div role="separator" className="tw:border-b tw:border-lm-border tw:dark:border-dm-border tw:my-2" />
 );
 
 const Title: FC<PropsWithChildren> = ({ children }) => (
   <div
+    // TODO Use an <h6 /> tag once tailwind styles are not set with !important
     role="heading"
-    aria-level={4}
+    aria-level={6}
     className={clsx(
       'tw:flex tw:items-center tw:w-full tw:px-3 tw:py-1.5',
-      'tw:text-gray-500 tw:dark:text-gray-400 tw:text-sm tw:font-semibold',
+      'tw:text-gray-500 tw:text-sm tw:font-semibold',
     )}
   >
     {children}
