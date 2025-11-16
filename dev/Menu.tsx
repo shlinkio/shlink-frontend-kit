734cf300f34a70d532f1ea51feab984c1236ab93
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-11-16 11:57:46 +0100

    Use dropdown menu in dev sandbox

--- dev/Menu.tsx
+++ dev/Menu.tsx
@@ -1,28 +1,39 @@
 import type { FC } from 'react';
-import { Link } from 'react-router';
+import { useLocation } from 'react-router';
+import { Dropdown } from '../src';
+
+const routes: Record<string, string> = {
+  '/form/inputs': 'Inputs',
+  '/form/search-input': 'SearchInput',
+  '/form/search-combobox': 'SearchCombobox',
+  '/form/buttons': 'Buttons',
+  '/surfaces/cards': 'Cards',
+  '/content/tables': 'Tables',
+  '/content/details': 'Details',
+  '/navigation/paginator': 'Paginator',
+  '/navigation/nav-bar': 'NavBar',
+  '/navigation/nav-pills': 'NavPills',
+  '/navigation/menu': 'Menu',
+  '/navigation/dropdown': 'Dropdown',
+  '/feedback/dialogs': 'Dialogs',
+  '/feedback/tooltip': 'Tooltip',
+  '/feedback/result': 'Result',
+  '/feedback/message': 'Message',
+  '/utils': 'Utils',
+} as const;
+const routeEntries = Object.entries(routes);
 
 export const Menu: FC = () => {
+  const { pathname } = useLocation();
+  const text = routes[pathname] ?? 'Home';
+
   return (
-    <ul className="p-0">
-      <li><Link to="/">Home</Link></li>
-      <hr />
-      <li><Link to="/form/inputs">Inputs</Link></li>
-      <li><Link to="/form/search-input">SearchInput</Link></li>
-      <li><Link to="/form/search-combobox">SearchCombobox</Link></li>
-      <li><Link to="/form/buttons">Buttons</Link></li>
-      <li><Link to="/surfaces/cards">Cards</Link></li>
-      <li><Link to="/content/tables">Tables</Link></li>
-      <li><Link to="/content/details">Details</Link></li>
-      <li><Link to="/navigation/paginator">Paginator</Link></li>
-      <li><Link to="/navigation/nav-bar">NavBar</Link></li>
-      <li><Link to="/navigation/nav-pills">NavPills</Link></li>
-      <li><Link to="/navigation/menu">Menu</Link></li>
-      <li><Link to="/navigation/dropdown">Dropdown</Link></li>
-      <li><Link to="/feedback/dialogs">Dialogs</Link></li>
-      <li><Link to="/feedback/tooltip">Tooltip</Link></li>
-      <li><Link to="/feedback/result">Result</Link></li>
-      <li><Link to="/feedback/message">Message</Link></li>
-      <li><Link to="/utils">Utils</Link></li>
-    </ul>
+    <Dropdown buttonContent={text} containerClassName="w-full" buttonClassName="w-full">
+      <Dropdown.Item to="/" selected={pathname === '/'}>Home</Dropdown.Item>
+      <Dropdown.Separator />
+      {routeEntries.map(([path, name]) => (
+        <Dropdown.Item to={path} key={path} selected={pathname === path}>{name}</Dropdown.Item>
+      ))}
+    </Dropdown>
   );
 };
