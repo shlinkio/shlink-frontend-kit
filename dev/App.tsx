37d2e15de01fedaee4dbce578837e29f7496a3f5
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-01 18:54:17 +0100

    Remove docs for components already in storybook

--- dev/App.tsx
+++ dev/App.tsx
@@ -1,15 +1,5 @@
 import type { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router';
-import { DetailsPage } from './content/DetailsPage';
-import { TablePage } from './content/TablePage';
-import { MessagePage } from './feedback/MessagePage';
-import { ModalDialogPage } from './feedback/ModalDialogPage';
-import { ResultPage } from './feedback/ResultPage';
-import { TooltipPage } from './feedback/TooltipPage';
-import { ButtonsPage } from './form/ButtonsPage';
-import { InputsPage } from './form/InputsPage';
-import { SearchComboboxPage } from './form/SearchComboboxPage';
-import { SearchInputPage } from './form/SearchInputPage';
 import { Menu } from './Menu';
 import { DropdownPage } from './navigation/DropdownPage';
 import { MenuPage } from './navigation/MenuPage';
@@ -32,13 +22,7 @@ export const App: FC = () => (
       </div>
       <Routes>
         <Route path="/" element={<h2 className="text-center">Select component</h2>} />
-        <Route path="/form/inputs" element={<InputsPage />} />
-        <Route path="/form/search-input" element={<SearchInputPage />} />
-        <Route path="/form/search-combobox" element={<SearchComboboxPage />} />
-        <Route path="/form/buttons" element={<ButtonsPage />} />
         <Route path="/surfaces/cards" element={<CardsPage />} />
-        <Route path="/content/tables" element={<TablePage />} />
-        <Route path="/content/details" element={<DetailsPage />} />
         <Route path="/navigation/paginator" element={<PaginatorPage />} />
         <Route path="/navigation/nav-bar" element={<NavBarPage />} />
         <Route path="/navigation/nav-pills">
@@ -47,10 +31,6 @@ export const App: FC = () => (
         </Route>
         <Route path="/navigation/menu" element={<MenuPage />} />
         <Route path="/navigation/dropdown" element={<DropdownPage />} />
-        <Route path="/feedback/dialogs" element={<ModalDialogPage />} />
-        <Route path="/feedback/result" element={<ResultPage />} />
-        <Route path="/feedback/message" element={<MessagePage />} />
-        <Route path="/feedback/tooltip" element={<TooltipPage />} />
         <Route path="/utils" element={<UtilsPage />} />
         <Route path="*" element={<h2 className="text-center">Not found</h2>} />
       </Routes>
