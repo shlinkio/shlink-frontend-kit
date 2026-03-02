dec0a36e4c6b346235776eaa5c1b33175263f6d9
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-02 22:21:57 +0100

    Add Dropdown component storybook stories

--- dev/navigation/DropdownPage.tsx
+++ dev/navigation/DropdownPage.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
-import { useCallback , useState } from 'react';
+import { useState } from 'react';
 import type { Order } from '../../src';
-import { Dropdown, LabelledInput, OrderingDropdown, RowDropdown } from '../../src';
+import { OrderingDropdown, RowDropdown } from '../../src';
 
 const items = {
   foo: 'Foo',
@@ -10,105 +10,11 @@ const items = {
 };
 
 export const DropdownPage: FC = () => {
-  const [selected, setSelected] = useState<string>();
-  const toggleSelected = useCallback((value: string) => setSelected((prev) => prev === value ? undefined : value), []);
   const [order, onChange] = useState<Order<keyof typeof items>>({});
   const [orderLink, onChangeLink] = useState<Order<keyof typeof items>>({});
 
   return (
     <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col gap-y-2">
-        <h2>Dropdown</h2>
-        <div>
-          <Dropdown buttonContent={selected ?? <i>Click me</i>}>
-            <Dropdown.Item selected={selected === 'Foo'} onClick={() => toggleSelected('Foo')}>Foo</Dropdown.Item>
-            <Dropdown.Item selected={selected === 'Bar'} onClick={() => toggleSelected('Bar')}>Bar</Dropdown.Item>
-            <Dropdown.Item selected={selected === 'Baz'} onClick={() => toggleSelected('Baz')}>Baz</Dropdown.Item>
-            <Dropdown.Separator />
-            <Dropdown.Title>More stuff</Dropdown.Title>
-            <Dropdown.Misc>
-              <div className="flex items-center gap-3 min-w-92">
-                <div className="w-1/2">
-                  <LabelledInput label="Foo" />
-                </div>
-                <div className="w-1/2">
-                  <LabelledInput label="Bar" />
-                </div>
-              </div>
-            </Dropdown.Misc>
-          </Dropdown>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h2>Menu at least as big as button</h2>
-        <div>
-          <Dropdown buttonContent="Select something from the menu">
-            <Dropdown.Item to="">Foo</Dropdown.Item>
-            <Dropdown.Item to="">Bar</Dropdown.Item>
-            <Dropdown.Item to="">Baz</Dropdown.Item>
-          </Dropdown>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h2 className="text-right">Right-aligned Dropdown</h2>
-        <div className="flex justify-end">
-          <Dropdown buttonContent="Right menu" menuAlignment="right">
-            <Dropdown.Item>Foo with a very long text</Dropdown.Item>
-            <Dropdown.Item>Bar with a very long text</Dropdown.Item>
-            <Dropdown.Item>Baz with a very long text</Dropdown.Item>
-          </Dropdown>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h2>Toggle button sizes</h2>
-        <div className="flex gap-3 flex-wrap items-center">
-          <Dropdown buttonContent="Small" buttonSize="sm">
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-          <Dropdown buttonContent="Medium (default)" buttonSize="md">
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-          <Dropdown buttonContent="Large" buttonSize="lg">
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h2>Variants</h2>
-        <div className="flex gap-3 flex-wrap items-center">
-          <Dropdown buttonContent="Caretless" caretless>
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-          <Dropdown buttonContent="Link" buttonVariant="link">
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-          <Dropdown buttonContent="Text" buttonVariant="text">
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-          <Dropdown buttonContent="Disabled" buttonDisabled>
-            <Dropdown.Item>Foo</Dropdown.Item>
-            <Dropdown.Item>Bar</Dropdown.Item>
-            <Dropdown.Item>Baz</Dropdown.Item>
-          </Dropdown>
-        </div>
-      </div>
-
       <div className="flex flex-col gap-y-2">
         <h2>RowDropdown</h2>
         <div className="flex gap-3 flex-wrap items-center">
