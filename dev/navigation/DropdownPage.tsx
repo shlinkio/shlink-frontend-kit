6bc40aaabeb64287b2a8f1a5099762476fd4447d
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-03 18:27:39 +0100

    Add RowDropdown component storybook stories

--- dev/navigation/DropdownPage.tsx
+++ dev/navigation/DropdownPage.tsx
@@ -1,7 +1,7 @@
 import type { FC } from 'react';
 import { useState } from 'react';
 import type { Order } from '../../src';
-import { OrderingDropdown, RowDropdown } from '../../src';
+import { OrderingDropdown } from '../../src';
 
 const items = {
   foo: 'Foo',
@@ -14,34 +14,11 @@ export const DropdownPage: FC = () => {
   const [orderLink, onChangeLink] = useState<Order<keyof typeof items>>({});
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex flex-col gap-y-2">
-        <h2>RowDropdown</h2>
-        <div className="flex gap-3 flex-wrap items-center">
-          <RowDropdown buttonSize="sm">
-            <RowDropdown.Item>Foo</RowDropdown.Item>
-            <RowDropdown.Item>Bar</RowDropdown.Item>
-            <RowDropdown.Item>Baz</RowDropdown.Item>
-          </RowDropdown>
-          <RowDropdown>
-            <RowDropdown.Item>Foo</RowDropdown.Item>
-            <RowDropdown.Item>Bar</RowDropdown.Item>
-            <RowDropdown.Item>Baz</RowDropdown.Item>
-          </RowDropdown>
-          <RowDropdown buttonSize="lg">
-            <RowDropdown.Item>Foo</RowDropdown.Item>
-            <RowDropdown.Item>Bar</RowDropdown.Item>
-            <RowDropdown.Item>Baz</RowDropdown.Item>
-          </RowDropdown>
-        </div>
-      </div>
-
-      <div className="flex flex-col gap-y-2">
-        <h2>OrderingDropdown</h2>
-        <div className="flex gap-3 flex-wrap items-center">
-          <OrderingDropdown items={items} order={order} onChange={onChange} />
-          <OrderingDropdown items={items} order={orderLink} onChange={onChangeLink} buttonVariant="link" />
-        </div>
+    <div className="flex flex-col gap-y-2">
+      <h2>OrderingDropdown</h2>
+      <div className="flex gap-3 flex-wrap items-center">
+        <OrderingDropdown items={items} order={order} onChange={onChange} />
+        <OrderingDropdown items={items} order={orderLink} onChange={onChangeLink} buttonVariant="link" />
       </div>
     </div>
   );
