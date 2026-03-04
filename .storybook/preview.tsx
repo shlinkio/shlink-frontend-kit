cfa0f18316f545712d952bd0d191d97b2d88f4d3
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-04 09:01:49 +0100

    Add tailwind theme selector to storybook docs

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
 import type { Preview } from '@storybook/react-vite';
+import { useLayoutEffect } from 'react';
 import { MemoryRouter } from 'react-router';
+import { changeThemeInMarkup, getSystemPreferredTheme } from '../src';
 import './tailwind.css';
 
 // eslint-disable-next-line no-restricted-exports
@@ -12,12 +14,43 @@ export default {
       },
     },
   },
+  globalTypes: {
+    theme: {
+      description: 'Theme for components',
+      toolbar: {
+        // The label to show for this toolbar item
+        title: 'Theme',
+        icon: 'circlehollow',
+        items: [
+          { title: 'light', icon: 'sun', value: 'light' },
+          { title: 'dark', icon: 'moon', value: 'dark' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
   decorators: [
-    // Wrap all stories in a memory router in case they use react-router components or hooks
-    (Story) => (
-      <MemoryRouter>
-        <Story />
-      </MemoryRouter>
-    ),
+    (Story, { globals }) => {
+
+      useLayoutEffect(() => {
+        // Add tailwind theme-based background classes to story wrapping elements
+        document.querySelectorAll('.docs-story').forEach((element) => {
+          element.classList.add('bg-lm-secondary', 'dark:bg-dm-secondary');
+        });
+      }, []);
+
+      const theme = globals.theme;
+      useLayoutEffect(() => {
+        // Apply new theme every time the value changes
+        changeThemeInMarkup(theme ?? getSystemPreferredTheme());
+      }, [theme]);
+
+      // Wrap all stories in a memory router in case they use react-router components or hooks
+      return (
+        <MemoryRouter>
+          <Story />
+        </MemoryRouter>
+      );
+    },
   ],
 } satisfies Preview;
