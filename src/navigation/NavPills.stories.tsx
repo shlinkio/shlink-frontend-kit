a7184d78a573a062f6da4a172703ad8d0131761e
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 13:30:16 +0100

    Normalize NavPills storybook stories

--- src/navigation/NavPills.stories.tsx
+++ src/navigation/NavPills.stories.tsx
@@ -1,19 +1,22 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import type { FC } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
+import { nonEditableReactNode } from '../../.storybook/utils/storybook';
+import type { NavPillsProps } from './NavPills';
 import { NavPills } from './NavPills';
 
-export default {
+const meta = {
   component: NavPills,
   tags: ['autodocs'],
 } satisfies Meta<typeof NavPills>;
 
-export const LeftAlign = () => (
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const Decorator = (Story: FC) => (
   <div className="flex flex-col gap-y-3">
-    <NavPills>
-      <NavPills.Pill to="foo">Foo</NavPills.Pill>
-      <NavPills.Pill to="bar">Bar</NavPills.Pill>
-      <NavPills.Pill to="baz">Baz</NavPills.Pill>
-    </NavPills>
+    <Story />
     <Routes>
       <Route path="foo" element={<p className="text-center">This is option foo</p>} />
       <Route path="bar" element={<p className="text-center">This is option bar</p>} />
@@ -23,18 +26,27 @@ export const LeftAlign = () => (
   </div>
 );
 
-export const Fill = () => (
-  <div className="flex flex-col gap-y-3">
-    <NavPills fill>
-      <NavPills.Pill to="foo">Foo</NavPills.Pill>
-      <NavPills.Pill to="bar">Bar</NavPills.Pill>
-      <NavPills.Pill to="baz">Baz</NavPills.Pill>
-    </NavPills>
-    <Routes>
-      <Route path="foo" element={<p className="text-center">This is option foo</p>} />
-      <Route path="bar" element={<p className="text-center">This is option bar</p>} />
-      <Route path="baz" element={<p className="text-center">This is option baz</p>} />
-      <Route path="*" element={<Navigate replace to="foo" />} />
-    </Routes>
-  </div>
+const Render = (args: NavPillsProps) => (
+  <NavPills {...args}>
+    <NavPills.Pill to="foo">Foo</NavPills.Pill>
+    <NavPills.Pill to="bar">Bar</NavPills.Pill>
+    <NavPills.Pill to="baz">Baz</NavPills.Pill>
+  </NavPills>
 );
+
+export const LeftAligned: Story = {
+  name: 'Left-aligned',
+  args: {
+    fill: false,
+  },
+  render: Render,
+  decorators: [Decorator],
+};
+
+export const Fill: Story = {
+  args: {
+    fill: true,
+  },
+  render: Render,
+  decorators: [Decorator],
+};
