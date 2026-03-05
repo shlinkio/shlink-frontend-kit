f451553cf9251cd4e51f2995a4f280599ce5b891
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 11:25:27 +0100

    Normalize Details storybook stories

--- src/content/Details.stories.tsx
+++ src/content/Details.stories.tsx
@@ -1,15 +1,28 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { LoremIpsum } from '../../.storybook/utils/LoremIpsum';
 import { Details } from './Details';
 
-export default {
+const meta = {
   component: Details,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
+  argTypes: {
+    children: {
+      control: false,
+      table: {
+        type: { summary: 'ReactNode' },
+      },
+    },
+  },
 } satisfies Meta<typeof Details>;
 
-export const DefaultSummary = () => (
-  <Details summary="Click to show">
-    <LoremIpsum repeat={3} />
-  </Details>
-);
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    summary: 'Click to toggle',
+    children: <LoremIpsum repeat={3} />,
+  },
+};
