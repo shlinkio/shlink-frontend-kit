32332d0e8a5db5e13e49e17ffb8a227154250a25
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 13:39:53 +0100

    Normalize CopyToClipboard storybook stories

--- src/utils/CopyToClipboardButton.stories.tsx
+++ src/utils/CopyToClipboardButton.stories.tsx
@@ -1,20 +1,30 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { useState } from 'react';
 import { Input } from '../form';
 import { CopyToClipboardButton } from './CopyToClipboardButton';
 
-export default {
+const meta = {
   component: CopyToClipboardButton,
   tags: ['autodocs'],
 } satisfies Meta<typeof CopyToClipboardButton>;
 
-export const Base = () => {
-  const [text, setText] = useState('Click to copy this text');
+export default meta;
 
-  return (
-    <div className="flex items-center gap-x-2">
-      <Input value={text} onChange={(e) => setText(e.target.value)} className="grow" />
-      <CopyToClipboardButton text={text} />
-    </div>
-  );
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    text: 'Text to copy',
+  },
+  render: (args) => {
+    const [text, setText] = useState('Click to copy this text');
+
+    return (
+      <div className="flex items-center gap-x-2">
+        <Input value={text} onChange={(e) => setText(e.target.value)} className="grow" />
+        <CopyToClipboardButton {...args} text={text} />
+      </div>
+    );
+  },
 };
+
