db0c5593ae98857dcbce48216a8f9a4e630554d7
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 12:47:56 +0100

    Normalize Select storybook stories

--- src/form/Select.stories.tsx
+++ src/form/Select.stories.tsx
@@ -1,47 +1,60 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { nonEditableReactNode, size } from '../../.storybook/utils/storybook';
+import type { SelectProps } from './Select';
 import { Select } from './Select';
 
-export default {
+const meta = {
   component: Select,
   tags: ['autodocs'],
+  argTypes: {
+    children: nonEditableReactNode,
+    size,
+  },
 } satisfies Meta<typeof Select>;
 
-export const Regular = () => (
-  <Select>
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const Render = (args: SelectProps) => (
+  <Select {...args}>
     <option>One</option>
     <option>Two</option>
     <option>Three</option>
   </Select>
 );
 
-export const Error = () => (
-  <Select feedback="error">
-    <option>One</option>
-    <option>Two</option>
-    <option>Three</option>
-  </Select>
-);
+export const Regular: Story = {
+  args: {
+    disabled: false,
+  },
+  render: Render,
+};
 
-export const Disabled = () => (
-  <Select disabled>
-    <option>One</option>
-    <option>Two</option>
-    <option>Three</option>
-  </Select>
-);
+export const Error: Story = {
+  args: {
+    feedback: 'error',
+  },
+  render: Render,
+};
 
-export const Small = () => (
-  <Select size="sm">
-    <option>One</option>
-    <option>Two</option>
-    <option>Three</option>
-  </Select>
-);
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: Render,
+};
 
-export const Large = () => (
-  <Select size="lg">
-    <option>One</option>
-    <option>Two</option>
-    <option>Three</option>
-  </Select>
-);
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+  render: Render,
+};
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
+  render: Render,
+};
