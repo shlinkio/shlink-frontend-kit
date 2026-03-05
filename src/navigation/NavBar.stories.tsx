77a90fb7c6e684a75f4da0b54d79fe700c13abdb
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 13:24:18 +0100

    Normalize NavBar storybook stories

--- src/navigation/NavBar.stories.tsx
+++ src/navigation/NavBar.stories.tsx
@@ -1,25 +1,43 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
 import { LoremIpsum } from '../../.storybook/utils/LoremIpsum';
+import { nonEditableReactNode } from '../../.storybook/utils/storybook';
 import { Dropdown } from './Dropdown';
 import { NavBar } from './NavBar';
 
-export default {
+const meta = {
   component: NavBar,
   tags: ['autodocs'],
+  argTypes: {
+    children: nonEditableReactNode,
+  },
 } satisfies Meta<typeof NavBar>;
 
-export const Basic = () => (
-  <div className="flex flex-col gap-y-2">
-    <NavBar brand={<>Shlink</>}>
-      <NavBar.MenuItem to="">Foo</NavBar.MenuItem>
-      <NavBar.MenuItem to="" active>Bar</NavBar.MenuItem>
-      <NavBar.MenuItem to="">Baz</NavBar.MenuItem>
-      <NavBar.Dropdown buttonContent="Options">
-        <Dropdown.Item>First option</Dropdown.Item>
-        <Dropdown.Item selected>Second option</Dropdown.Item>
-        <Dropdown.Item>Third option</Dropdown.Item>
-      </NavBar.Dropdown>
-    </NavBar>
-    <LoremIpsum repeat={3} />
-  </div>
-);
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {
+  args: {
+    brand: 'Shlink',
+    children: (
+      <>
+        <NavBar.MenuItem to="">Foo</NavBar.MenuItem>
+        <NavBar.MenuItem to="" active>Bar</NavBar.MenuItem>
+        <NavBar.MenuItem to="">Baz</NavBar.MenuItem>
+        <NavBar.Dropdown buttonContent="Options">
+          <Dropdown.Item>First option</Dropdown.Item>
+          <Dropdown.Item selected>Second option</Dropdown.Item>
+          <Dropdown.Item>Third option</Dropdown.Item>
+        </NavBar.Dropdown>
+      </>
+    ),
+  },
+  decorators: [
+    (Story) => (
+      <div className="flex flex-col gap-y-2">
+        <Story />
+        <LoremIpsum repeat={3} />
+      </div>
+    ),
+  ],
+};
