918e507558a5ac19cbf629e5768db653e3fcdf70
Alejandro Celaya <alejandrocelaya@gmail.com>
2026-03-05 13:34:16 +0100

    Normalize RowDropdown storybook stories

--- src/navigation/RowDropdown.stories.tsx
+++ src/navigation/RowDropdown.stories.tsx
@@ -1,10 +1,20 @@
-import type { Meta } from '@storybook/react-vite';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { size } from '../../.storybook/utils/storybook';
+import type { RowDropdownProps } from './RowDropdown';
 import { RowDropdown } from './RowDropdown';
 
-export default {
+const meta = {
   component: RowDropdown,
   tags: ['autodocs'],
+  argTypes: {
+    buttonLabel: { type: 'string' },
+    buttonSize: size,
+  },
 } satisfies Meta<typeof RowDropdown>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
 
 const items = {
   foo: 'Foo',
@@ -14,20 +24,27 @@ const items = {
 
 const itemsEntries = Object.entries(items);
 
-export const Medium = () => (
-  <RowDropdown>
+const Render = (args: RowDropdownProps) => (
+  <RowDropdown {...args}>
     {itemsEntries.map(([key, value]) => <RowDropdown.Item key={key}>{value}</RowDropdown.Item>)}
   </RowDropdown>
 );
 
-export const Small = () => (
-  <RowDropdown buttonSize="sm">
-    {itemsEntries.map(([key, value]) => <RowDropdown.Item key={key}>{value}</RowDropdown.Item>)}
-  </RowDropdown>
-);
+export const Base: Story = {
+  args: {},
+  render: Render,
+};
 
-export const Large = () => (
-  <RowDropdown buttonSize="lg">
-    {itemsEntries.map(([key, value]) => <RowDropdown.Item key={key}>{value}</RowDropdown.Item>)}
-  </RowDropdown>
-);
+export const Small: Story = {
+  args: {
+    buttonSize: 'sm',
+  },
+  render: Render,
+};
+
+export const Large: Story = {
+  args: {
+    buttonSize: 'lg',
+  },
+  render: Render,
+};
